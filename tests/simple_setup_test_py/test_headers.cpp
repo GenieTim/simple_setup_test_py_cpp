31fd9009cf16265d8e9ae452679d552e129140fa
Tim Bernhard <tim@bernhard-webstudio.ch>
2025-03-25 14:20:49 +0100

    Simplify some test to emphasize what's not working

--- tests/simple_setup_test_py/test_headers.cpp
+++ tests/simple_setup_test_py/test_headers.cpp
@@ -13,9 +13,8 @@ TEST_CASE("HEADER: TESTS ARE RUN", "[general]")
   REQUIRE(1 == 2 - 1);
 }
 
-TEST_CASE("HEADER: Segment Intersection is found", "[TopologyCalc]")
+TEST_CASE("Eigen Vectors are constructed", "[TopologyCalc]")
 {
-  REQUIRE(1 == 2 - 1);
   Eigen::Vector3d vertex1;
   vertex1 << -1.0, -1.0, 0.0;
   Eigen::Vector3d vertex2;
@@ -23,44 +22,13 @@ TEST_CASE("HEADER: Segment Intersection is found", "[TopologyCalc]")
   Eigen::Vector3d vertex3;
   vertex3 << 1.0, 1.0, 0.0;
 
-  SECTION("Intersection is found")
-  {
-    Eigen::Vector3d rayOrigin;
-    rayOrigin << 0.5, -0.5, -1.0;
-    Eigen::Vector3d rayTarget;
-    rayTarget << 0.5, -0.5, 1.0;
-
-    Eigen::Vector3d intersectionPoint;
-
-    REQUIRE(simple_setup_test_py::calc::segmentIntersectsTriangleWhere(
-      rayOrigin, rayTarget, vertex1, vertex2, vertex3, intersectionPoint));
-  }
-
-  SECTION("Intersection should not be found")
-  {
-    Eigen::Vector3d rayOrigin;
-    rayOrigin << -0.5, 0.5, -1.0;
-    Eigen::Vector3d rayTarget;
-    rayTarget << -0.5, 0.5, 1.0;
-
-    Eigen::Vector3d intersectionPoint;
-
-    REQUIRE_FALSE(simple_setup_test_py::calc::segmentIntersectsTriangleWhere(
-      rayOrigin, rayTarget, vertex1, vertex2, vertex3, intersectionPoint));
-  }
-
-  SECTION("Intersection should not be found in too short segment")
-  {
-    Eigen::Vector3d rayOrigin;
-    rayOrigin << 0.5, -0.5, -1.0;
-    Eigen::Vector3d rayTarget;
-    rayTarget << 0.5, -0.5, -2.0;
-
-    Eigen::Vector3d intersectionPoint;
-
-    REQUIRE_FALSE(simple_setup_test_py::calc::segmentIntersectsTriangleWhere(
-      rayOrigin, rayTarget, vertex1, vertex2, vertex3, intersectionPoint));
-    REQUIRE_FALSE(simple_setup_test_py::calc::segmentIntersectsTriangle(
-      rayOrigin, rayTarget, vertex1, vertex2, vertex3));
-  }
+  CHECK(vertex1.isApprox(Eigen::Vector3d(-1.0, -1.0, 0.0)));
+  CHECK(vertex1.isApprox(
+    simple_setup_test_py::cal::pointsToVector3d(-1.0, -1.0, 0.0)));
+  CHECK(vertex2.isApprox(Eigen::Vector3d(1.0, -1.0, 0.0)));
+  CHECK(vertex2.isApprox(
+    simple_setup_test_py::cal::pointsToVector3d(1.0, -1.0, 0.0)));
+  CHECK(vertex3.isApprox(Eigen::Vector3d(1.0, 1.0, 0.0)));
+  CHECK(vertex3.isApprox(
+    simple_setup_test_py::cal::pointsToVector3d(1.0, 1.0, 0.0)));
 }
